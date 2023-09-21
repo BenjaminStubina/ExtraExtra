11c392f2f6ee6515bca97898f0217ce26042bb77
bennygoldman <goldmanb@gmail.com>
2023-09-21 13:30:07 -0400

    add basic copy and page structure

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,27 @@
+const About = () => {
+    return (
+        <>
+            <p>ExtraExtra was created, designed, and developed in Toronto by Benjamin Stubina and Benny Goldman.</p>
+
+            <p>Benjamin and Benny are recent coding bootcamp grads seeking job opportunities.</p>
+
+            <p>Get in touch:</p>
+            <ul>
+                <li>Benjamin Stubina
+                    <ul>
+                        <li><a href="https://github.com/BenjaminStubina">Github</a></li>
+                        <li><a href="https://www.linkedin.com/in/benjamin-stubina/">LinkedIn</a></li>
+                    </ul>
+                </li>
+                <li>Benny Goldman
+                    <ul>
+                        <li><a href="https://github.com/bennygoldman">Github</a></li>
+                        <li><a href="https://www.linkedin.com/in/bennygoldman/">LinkedIn</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </>
+    );
+};
+
+export default About;
