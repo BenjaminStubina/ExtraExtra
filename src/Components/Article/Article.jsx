1f8eef953bc35dfa1aa0d49440512e89a837d7a8
Benjamin Stubina <benjamin.stubina@gmail.com>
2023-10-05 11:17:45 -0400

    Add hover effect to individual articles

--- src/Components/Article/Article.jsx
+++ src/Components/Article/Article.jsx
@@ -3,7 +3,7 @@ const Article = ({ article, image, link }) => {
         <div className="flex h-[304px] border-solid border-2 hover:border-4 hover:border-red-500">
             <a href={link} target='_'>
                 <div className="flex w-full self-center justify-center">
-                    <img src={image} alt='' className="object-contain h-[300px] w-[300px]" />
+                    <img src={image} alt='' className="object-contain h-[300px] w-[300px] hover:h-[296px] brightness-[0.95] hover:brightness-[1.2]" />
                 </div>
             </a>
         </div>
