35223ac6551f4c02868313a87cb146d450160413
bennygoldman <goldmanb@gmail.com>
2023-12-08 14:54:46 -0500

    Cropped images for square. Rounded border.

--- Client/src/Components/Article/Article.jsx
+++ Client/src/Components/Article/Article.jsx
@@ -2,14 +2,12 @@ const Article = ({ article, image, link }) => {
 
     // Individually mapped article
     return (
-        <article className="flex h-[300px] w-[300px] border-solid border-[8px] border-transparent md:hover:border-red-500">
+        <article className="flex h-[300px] w-[300px] border-solid border-[8px] border-transparent md:hover:border-red-500 rounded">
             <a href={link} target='_' className="w-[284px] h-[284px]">
-                <div className="flex justify-center w-[284px]">
-                    <img src={image} alt='' className="absolute object-contain h-[284px] w-[284px] brightness-[0.9] hover:brightness-[1.1]" />
-                </div>
+                <img src={image} alt='' className="object-cover object-center h-[284px] w-[284px] brightness-[0.9] hover:brightness-[1.1]" />
             </a>
         </article>
-    )
-}
+    );
+};
 
 export default Article;
