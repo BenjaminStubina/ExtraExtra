0b37285fb3b5b7fac97e0e93739cdc0081c7972a
bennygoldman <goldmanb@gmail.com>
2023-12-08 15:02:59 -0500

    Fixed links in AboutDevs. Minor tweaks to bio info

--- Client/src/Components/About/AboutUs.jsx
+++ Client/src/Components/About/AboutUs.jsx
@@ -1,8 +1,8 @@
-import { Dialog } from '@mui/material'
+import { Dialog } from '@mui/material';
 import './ModalStyle.scss';
 import Techlist from '../TechList/Techlist';
-import github from '../../Assets/Images/techIcons/github.png'
-import linkedin from '../../Assets/Images/techIcons/123.png'
+import github from '../../Assets/Images/techIcons/github.png';
+import linkedin from '../../Assets/Images/techIcons/123.png';
 
 
 const AboutUs = ({ openAboutUs, handleCloseAboutUs }) => {
@@ -17,9 +17,10 @@ const AboutUs = ({ openAboutUs, handleCloseAboutUs }) => {
         }} open={openAboutUs} onClose={handleCloseAboutUs} className='w-full'>
             <section className='text-[20px] md:text-[22px] xl:text-[32px] p-4 md:p-6 xl:p-8 gap-2 md:gap-4 xl:gap-6'>
                 <p className='text-[22px] md:text-[24px] xl:text-[34px] underline'>
-                        About:
+                    About:
                 </p>
-                <p className=''>ExtraExtra was created, designed, and developed in Toronto by Benjamin Stubina and Benny Goldman. Benjamin and Benny are recent coding bootcamp grads seeking job opportunities.</p> 
+                <p>ExtraExtra was created in Toronto by Benjamin Stubina and Benny Goldman.</p>
+                <p>We're career-switchers, bootcamp grads, and available for hire.</p>
                 <div className='flex flex-col gap-2 md:gap-4 my-2 md:my-4 xl:my-6'>
                     <p className='text-[22px] md:text-[24px] xl:text-[34px] underline'>
                         Techstack:
@@ -34,17 +35,17 @@ const AboutUs = ({ openAboutUs, handleCloseAboutUs }) => {
                         <li>Benjamin Stubina
                             <ul className='flex flex-col'>
                                 <li>
-                                    <a className='flex gap-2 items-center' href="https:github.com/BenjaminStubina" target="_">
-                                        <img src={github} className="w-[24px] md:w-[30px] xl:w-[40px]" />   
+                                    <a className='flex gap-2 items-center' href="https://github.com/BenjaminStubina" target="_">
+                                        <img src={github} className="w-[24px] md:w-[30px] xl:w-[40px]" />
                                         <p className=''>
                                             Github
                                         </p>
                                     </a></li>
                                 <li>
-                                    <a className='flex gap-2 items-center' href="https:www.linkedin.com/in/benjamin-stubina/" target="_">
+                                    <a className='flex gap-2 items-center' href="https://www.linkedin.com/in/benjamin-stubina/" target="_">
                                         <img src={linkedin} className="w-[24px] md:w-[30px] xl:w-[40px]" />
                                         <p className=''>
-                                            LinkedIn 
+                                            LinkedIn
                                         </p>
                                     </a></li>
                             </ul>
@@ -52,14 +53,14 @@ const AboutUs = ({ openAboutUs, handleCloseAboutUs }) => {
                         <li>Benny Goldman
                             <ul className='flex flex-col'>
                                 <li>
-                                    <a className='flex gap-2 items-center' href="https:github.com/bennygoldman" target="_">
+                                    <a className='flex gap-2 items-center' href="https://github.com/bennygoldman" target="_">
                                         <img src={github} className="w-[24px] md:w-[30px] xl:w-[40px]" />
                                         <p className=''>
-                                            Github 
+                                            Github
                                         </p>
                                     </a></li>
                                 <li>
-                                    <a className='flex gap-2 items-center' href="https:www.linkedin.com/in/bennygoldman/" target="_">
+                                    <a className='flex gap-2 items-center' href="https://www.linkedin.com/in/bennygoldman/" target="_">
                                         <img src={linkedin} className="w-[24px] md:w-[30px] xl:w-[40px]" />
                                         <p className=''>
                                             LinkedIn
@@ -71,7 +72,7 @@ const AboutUs = ({ openAboutUs, handleCloseAboutUs }) => {
                 </div>
             </section>
         </Dialog>
-    )
-}
+    );
+};
 
-export default AboutUs
+export default AboutUs;
