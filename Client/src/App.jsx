1491353a20f39b3b86be79bf2a03471366123985
BenjaminStubina <benjamin.stubina@gmail.com>
2024-01-03 19:35:38 -0500

    Fix cap

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 
-import Layout from './components/Layout';
+import Layout from './Components/Layout';
 import Home from './pages/Home/Home';
 import Project from './pages/Project/';
 import Resources from './pages/Resources';
