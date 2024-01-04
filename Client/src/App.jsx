db089735bec335309b1cd4911efc2b9ad689735b
BenjaminStubina <benjamin.stubina@gmail.com>
2024-01-03 19:29:47 -0500

    Resolve merge conflicts

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-import Header from './Components/Header/Header';
-import Home from './pages/Home/Home';
-=======
 import { Routes, Route } from 'react-router-dom';
 
 import Layout from './components/Layout';
@@ -9,7 +5,6 @@ import Home from './pages/Home';
 import Project from './pages/Project';
 import Resources from './pages/Resources';
 
->>>>>>> e5e540a0326301c22e8acaa394de721cf81805bc
 
 function App() {
 
