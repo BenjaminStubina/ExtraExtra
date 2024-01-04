9f6fa38511ab2b19f556a62525e5c6fe2dd55d69
bennygoldman <goldmanb@gmail.com>
2024-01-04 01:55:25 -0500

    Style - update Page Title styling

--- Client/src/pages/Home/Home.jsx
+++ Client/src/pages/Home/Home.jsx
@@ -21,18 +21,19 @@ export default function Home() {
 
   return (
     <>
-      <PageTitle
-        as="hgroup"
-        className={`flex flex-col w-full items-center mx-2`}
-      >
+      <PageTitle as="hgroup" className={`mt-2 mb-4`}>
         <h1
-          className={`italic text-center font-medium max-w-sm xs:min-w-max text-3xl ${`md:min-w-max md:text-4xl lg:text-5xl xl:text-6xl`}`}
+          className={`mb-2 italic text-center font-medium mx-auto max-w-sm text-3xl ${`xs:min-w-max xs:text-2xl xm:text-3xl md:text-4xl lg:mb-4 lg:text-5xl xl:text-6xl`}`}
         >
           NEWS RETURNS TO SOCIAL MEDIA
         </h1>
-        <p className="font-headline min-w-0 w-full text-center _max-w-max text-xl font-medium md:text-3xl md:font-normal lg:text-4xl xl:text-5xl">
+        <p
+          className={`font-headline text-xl font-medium ml-4 ${`xm:ml-36 md:ml-48 lg:ml-72 xl:ml-80 xm:text-2xl md:text-3xl md:font-normal lg:text-4xl xl:text-5xl`}`}
+        >
           Canadians rejoice
-          <span className="text-xs md:font-light lg:text-sm font-normal hover:underline">
+          <span
+            className={`font-serif text-xs hover:underline ml-1 ${`xm:ml-12 md:ml-20 lg:ml-28 xl:ml-52 md:font-light lg:text-sm`}`}
+          >
             <Link to="about"> See HEROES, Page A2</Link>
           </span>
         </p>
